b246b02dc09ea8e1bc3f3616fca06e89444eea2b
BECERRA DELGADO Jairo Antonio <jairoantonio.becerradelgado@amaris.com>
2025-12-01 11:29:18 -0500

    feat: progress services

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => {
+    return {
+      secret: configService.get('JWT_SECRET'),
+      signOptions: {
+        expiresIn: '2h',
+      },
+    };
+  },
+
 })
-export class AuthModule {}
+export class AuthModule { }
