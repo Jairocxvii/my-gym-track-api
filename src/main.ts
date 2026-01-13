23804d4d8661ef6d02102ef09ca1492f3caaa9a7
BECERRA DELGADO Jairo Antonio <jairoantonio.becerradelgado@amaris.com>
2026-01-13 11:42:54 -0500

    add: postman json

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,15 @@ async function bootstrap() {
       },
     }),
   );
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+    ],
 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
